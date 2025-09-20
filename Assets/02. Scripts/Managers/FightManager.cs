6dc8f86019ac906a9a9bc45dfa376d2d499e133b
Gang <kingrkddn@gmail.com>
2025-09-20 18:26:56 +0900

    인트로씬에서캐릭터무버해제및싱글룸에서활성화

---
+++ Assets/02. Scripts/Managers/FightManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using UnityEngine;
+
+public class FightManager : MonoBehaviour
+{
+    [SerializeField] GameObject AIplayer;
+    [SerializeField] GameObject AIomok;
+
+    public IEnumerator AIplayerAppear()
+    {
+        AIomok.SetActive(false);
+        yield return new WaitForSeconds(3.6f);
+
+        Instantiate(AIplayer, new Vector3(1f, 0.5f, 1.5f), Quaternion.Euler(0f, 180f, 0f));
+
+        BossAI bossAI = FindFirstObjectByType<BossAI>();
+
+
+        if (bossAI != null)
+        {
+            bossAI.enabled = false;
+
+            yield return new WaitForSeconds(5f);
+
+            Animator aiAnimator = bossAI.GetComponent<Animator>();
+            if (aiAnimator != null)
+            {
+                aiAnimator.SetTrigger("StartFight");
+            }
+
+            bossAI.enabled = true;
+        }
+        else
+        {
+            Debug.LogError("씬에서 생성된 BossAI를 찾을 수 없습니다!");
+        }
+    }
+
+    public IEnumerator EndBoxing()
+    {
+        yield return new WaitForSeconds(2f);
+        Destroy(GameObject.FindWithTag("Boss"));
+        AIomok.SetActive(true);
+    }
+}
---
+++ Assets/02. Scripts/Managers/GameManage.cs
@@ -0,0 +1,65 @@
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using System;
+using TMPro;
+
+public class GameManage : Singleton<GameManage>
+{
+    //코드 담당자: 최은주
+    [SerializeField] Canvas canvas;
+    [SerializeField] GameObject noticeUI;   
+  
+    public delegate void OnCustom();
+    public event OnCustom onCustom;
+ 
+    public int gameNumber; // 1이 복싱 0이 오목
+
+    public int loginCount; //로그인 되면 1, 로그인 아닐 시에 0   
+
+    //스코어 매니저 참조해서 승패 팝업           
+
+    private void Awake()
+    {
+        onCustom += ChangeToGameScene;        
+    } 
+
+    public void SinglePlay()
+    {
+        onCustom?.Invoke();
+    }
+
+    private void ChangeToGameScene() //게임씬으로
+    {
+        SceneManager.LoadScene("Single Room");        
+    }    
+    public void ChangeToIntroScene()
+    {
+        SceneManager.LoadScene("MergeIntro");
+    }
+
+    public void ChangeToMulti()
+    {
+        SceneManager.LoadScene("Multi Room");
+    }
+
+    public void OpenNoticePanel(string message) //안내문구 팝업 인스턴스 생성
+    {
+        if (canvas != null)
+        {
+            var noticePanel = Instantiate(noticeUI, canvas.transform);
+            noticePanel.GetComponent<NoticePanel>().Notice(message);
+            StartCoroutine(noticePanel.GetComponent<NoticePanel>().Hide());
+        }
+    }
+
+    protected override void OnSceneLoad(Scene scene, LoadSceneMode mode)
+    {
+        //씬 로드
+    }
+
+    public void OnApplicationQuit()
+    {
+        //카운트 0으로 초기화
+        loginCount = 0; 
+    }
+}
